00d4e488a00717f0a8f3fa56921215f0d69fa32f
gressler <gressler@cds.eb.mil.br>
2016-09-08 15:37:58 -0300

    Comparador de objetos

---
+++ src/Diferenca.java
@@ -0,0 +1,22 @@
+
+public class Diferenca {
+	private String propriedade;
+	private Object valorNovo;
+	private Object valorVelho;
+	public Diferenca(String propriedade, Object valorNovo, Object valorVelho) {
+		super();
+		this.propriedade = propriedade;
+		this.valorNovo = valorNovo;
+		this.valorVelho = valorVelho;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Diferenca [propriedade=" + propriedade + ", valorNovo=" + valorNovo + ", valorVelho=" + valorVelho
+				+ "]";
+	}
+	
+	
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,49 @@
+
+public class Pessoa {
+	private String nome;
+	private String sobrenome;
+	private int idade;
+
+	public Pessoa(String nome, String sobrenome, int idade) {
+		super();
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.idade = idade;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the sobrenome
+	 */
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	/**
+	 * @param sobrenome the sobrenome to set
+	 */
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	/**
+	 * @return the idade
+	 */
+	public int getIdade() {
+		return idade;
+	}
+	/**
+	 * @param idade the idade to set
+	 */
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
